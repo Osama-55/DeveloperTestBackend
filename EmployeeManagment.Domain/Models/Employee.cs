f50f2251cc1153ae7ce92fad6b6bc8557aaaa7a4
Osama <mr1.osma115@outlook.sa>
2021-11-23 09:42:26 +0200

    first commit

---
+++ EmployeeManagment.Domain/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Domain.Models
+{
+    [Table(nameof(Employee))]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ EmployeeManagment.Domain/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Domain.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        void AddEmployee(Models.Employee employee);
+        List<Employee> GetIndex();
+    }
+}
